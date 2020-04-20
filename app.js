d0ece1e306e659108548371fe2841d15b2b5192e
RahulNathani49 <nathanirahul49@gmail.com>
2020-04-20 13:24:22 +0530

    changes

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plane' });
-    var myReadstream = fs.createReadStream(__dirname + '/Readme.txt', 'utf8');
+    var myReadstream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
     // WRITING ON SERVER
     myReadstream.pipe(res);
     console.log("Requested Url : ", req.url);
 
 });
-var rdstr = fs.createReadStream(__dirname + "/Readme.txt", 'utf8');
+var rdstr = fs.createReadStream(__dirname + "/readme.txt", 'utf8');
 var wrstr = fs.createWriteStream(__dirname + "/Writeme.txt", 'utf8');
 rdstr.on('data', function(chunk) {
     // WRITING ON DIRECTORY FILE
